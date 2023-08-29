f99c84529989afaac9ee7434d426c19e8731e518
Didrik Nordström <didrik.nordstrom@gmail.com>
2023-08-29 01:42:23 -0700

    Docs: shorter package-level docs

--- doc.go
+++ doc.go
@@ -1,8 +1,3 @@
-// Zeroconf is a pure Golang library for discovering and publishing services on the local network.
-//
-// It is tested on Windows, macOS and Linux and is compatible with Avahi, Bonjour, etc.
-// It implements:
-//
-// - RFC 6762: Multicast DNS (mDNS)
-// - RFC 6763: DNS Service Discovery (DNS-SD)
+// Zeroconf is a pure Golang library for discovering and publishing services on the local network
+// using mDNS (RFC 6762) and DNS-SD (RFC 6763).
 package zeroconf
